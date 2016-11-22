2dff430a247c1b05bcd958cb3e15dfb2d4c1fa5c
Astinis <Astiniska@gmail.com>
2016-11-22 16:21:09 +0200

    update calculate

--- src/Les1/Calculate.java
+++ src/Les1/Calculate.java
@@ -1,7 +1,37 @@
 package Les1;
 
+
+
 /**
- * Created by Борода on 22.11.2016.
+ Created by Борода on 22.11.2016.
+ класс реализует калькулятор
  */
 public class Calculate {
+    //результат вычисления
+    private int result;
+
+    /**
+     суммируем аргументы.
+     @param params Аргументы суммирования
+     */
+    public void add(int ... params){
+        for (Integer param : params)
+            this.result += param;
+    }
+
+    /**
+     * получить результат
+     * @return результат вычисления
+     */
+    public int getResult(){
+        return this.result;
+    }
+
+    /**
+     * очистить результат вычисления
+     */
+    public void cleanResult(){
+        this.result = 0;
+    }
+
 }
